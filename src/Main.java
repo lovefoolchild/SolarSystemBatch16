c45bcc2400af2de5c0876933d60531e65b69702f
lovefoolchild <lmrhenson@gmail.com>
2023-07-08 13:15:03 -0400

    The Feature Sun has been added successfully

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Omnax.Sun="1";
         Omnax.Planets="2";
         Omnax.Star="4";
+
+        FeatureSun Xeon = new FeatureSun();
+        Xeon.Name="treo";
+        Xeon.size="-32432km";
+        Xeon.temp="-344449843 celcius";
     }
 }
